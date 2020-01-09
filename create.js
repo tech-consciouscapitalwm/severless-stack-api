79f3324ee75a41a24050c1676a0dbc2145fada59
Curt Downing <tech@consciouscapitalwm.com>
2020-01-09 11:07:37 -0500

    Setting up our serverless infrastucture

--- create.js
+++ create.js
@@ -1,9 +1,9 @@
-import uuid from 'uuid'
-import * as dynamoDbLib from './libs/dynamodb-lib'
-import { success, failure } from './libs/response-lib'
+import uuid from 'uuid';
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
 
 export async function main (event, context) {
-  const data = JSON.parse(event.body)
+  const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
     Item: {
@@ -13,13 +13,13 @@ export async function main (event, context) {
       attachment: data.attachment,
       createdAt: Date.now()
     }
-  }
+  };
 
   try {
-    await dynamoDbLib.call('put', params)
-    return success(params.Item)
+    await dynamoDbLib.call('put', params);
+    return success(params.Item);
   } catch (e) {
-    console.log(e)
-    return failure({ status: false })
-  }
-}
+    console.log(e);
+    return failure({ status: false });
+  };
+};
--- tests/billing.test.js
+++ tests/billing.test.js
@@ -1,4 +1,4 @@
-import { calculateCost } from "../libs/billing-lib";
+import { calculateCost } from '../libs/billing-lib';
 
 test("Lowest tier", () => {
   const storage = 10;
